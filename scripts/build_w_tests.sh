23a089e379924ce35ba6962d1262bf42dc6fde2a
Akumanory <thesteelgrace@gmail.com>
2024-10-06 21:19:12 +0300

    [scripts] Refactor scripts

--- scripts/build_w_tests.sh
+++ scripts/build_w_tests.sh
@@ -9,8 +9,17 @@ cd "$(dirname "$0")"
 
 # echo "$PWD"
 
-cmake -B ../build/Debug -S .. -D CMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc-12 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_TOOLCHAIN_FILE="./conan_modules/debug/conan_toolchain.cmake"
-cmake --build ../build/Debug --target all -t test
+cmake \
+    -B ../build/Debug \
+    -S .. \
+    -D CMAKE_BUILD_TYPE:STRING=Debug \
+    -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc-12 \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    -DCMAKE_TOOLCHAIN_FILE="./conan_modules/debug/conan_toolchain.cmake"
+cmake \
+    --build ../build/Debug \
+    --target all \
+    -t test
 
 
 
--- scripts/conan_build.sh
+++ scripts/conan_build.sh
@@ -2,4 +2,11 @@
 
 cd "$(dirname "$0")"
 
-conan install ../conanfile.py -sbuild_type=Debug -of=../conan_modules/debug --profile=./configs/default_conan_profile --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+conan install \
+    ../conanfile.py \
+    -sbuild_type=Debug \
+    -of=../conan_modules/debug \
+    --profile:all=./configs/default_conan_profile \
+    --build=missing \
+    -c tools.system.package_manager:mode=install \
+    -c tools.system.package_manager:sudo=True
